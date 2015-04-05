c5f65be6dd7832096b524498cd76f0960989dc7c
Tao Yang <twofei@twofei.com>
2015-04-06 03:19:22 +0800

    我居然把博客软件名字写错了！

--- theme/footer.php
+++ theme/footer.php
@@ -1,7 +1,7 @@
 	</section>
 	<footer id="footer" class="mono">
 		<div style="text-align: center;">&copy; <?php echo date('Y'); ?> 女孩不哭 All rights reserved.</div>
-		<div style="text-align: center;">Designed by twofei. Proudly powered by <a target="_blank" style="color: inherit;" href="">TabBlog</a>.</div>
+		<div style="text-align: center;">Designed by twofei. Proudly powered by <a target="_blank" style="color: inherit;" href="">TaoBlog</a>.</div>
 	</footer>
 </div>
 
