17ab449183bceb20571b9cdbb1af5040242ba593
root <root@twofei.com>
2015-04-06 11:28:15 -0800

    no track of config.php

--- setup/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('TBPATH',		dirname(__FILE__).'/../');
-
-define('DB_HOST', 		'localhost');
-define('DB_USER', 		'user');
-define('DB_PASSWORD', 	'passwd');
-define('DB_NAME', 		'database');
-
