9e525eda30c76193872987160d9f61537c47a118
Tao Yang <twofei@twofei.com>
2015-04-16 18:48:58 +0800

    启用移动设备显示支持。

--- theme/header.php
+++ theme/header.php
@@ -6,6 +6,7 @@
 <html lang="zh-CN">
 <head>
 	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<title><?php
 		if(!$tbquery->count) {
 			echo '404';
