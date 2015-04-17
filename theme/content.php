9fdb54bbfb8926d8cb516bd8f58861aff55ff4c2
movsb <anhbk@qq.com>
2015-04-18 00:48:00 +0800

    增加改变字体大小的两个按钮

--- theme/content.php
+++ theme/content.php
@@ -4,9 +4,9 @@
 	</div>
 
 	<div class="meta">
-		<span><i class="fa fa-mr fa-user"></i>作者: 女孩不哭</span>
-		<span><i class="fa fa-mr fa-calendar"></i>日期: <?php echo preg_split('/ /', $the->date)[0]; ?></span>
-		<span class="category"><i class="fa fa-mr fa-folder"></i>分类: <?php 
+		<span class="item"><i class="fa fa-mr fa-user"></i>作者: 女孩不哭</span>
+		<span class="item"><i class="fa fa-mr fa-calendar"></i>日期: <?php echo preg_split('/ /', $the->date)[0]; ?></span>
+		<span class="item category"><i class="fa fa-mr fa-folder"></i>分类: <?php 
 			$taxes = $tbtax->tree_from_id($the->taxonomy);
 			$links = $tbtax->link_from_slug($taxes);
 
@@ -18,6 +18,22 @@
 			echo implode(',', $link_anchors);
 
 			?></span>
+		<span class="item font-sizing">
+			<i class="fa fa-mr fa-font"></i><span>字号: </span><?php
+			?><span class="dec"><i class="fa fa-minus"></i></span><?php
+			?><span class="inc"><i class="fa fa-plus"></i></span>
+		</span>
+		<script>
+			$('.font-sizing').click(function(e){
+				var post = $('.entry');
+				var cl = e.target.classList;
+				if(cl.contains('inc') || cl.contains('fa-plus')) {
+					post.css('font-size', parseFloat(post.css('font-size')) * 1.2 + 'px');
+				} else if(cl.contains('dec') || cl.contains('fa-minus')) {
+					post.css('font-size', Math.max(8, parseFloat(post.css('font-size')) / 1.2) + 'px');
+				}
+			});
+		</script>
 	</div>
 
 	<div class="entry">
