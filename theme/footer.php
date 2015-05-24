610686a0b150ed2f09e0bad1d3e60005a27b0902
movsb <anhbk@qq.com>
2015-05-25 03:29:31 +0800

    增加了“回到顶端”

--- theme/footer.php
+++ theme/footer.php
@@ -45,6 +45,23 @@
 <div style="display: none;">
 	<script src="http://js.users.51.la/17768957.js"></script>
 </div>
+<div class="back-to-top" id="back-to-top" title="回到顶端">
+	<i class="fa fa-arrow-circle-up"></i>
+	<script>
+		window.onscroll = function() {
+			if(window.scrollY > 160) {
+					$("#back-to-top").fadeIn(500);
+				} else {
+					$("#back-to-top").fadeOut(500);
+				}
+			};
+		$('#back-to-top').click(function(){
+			$('html').animate({
+				scrollTop: 0
+			}, 300);
+		});
+	</script>
+</div>
 </body>
 </html>
 
