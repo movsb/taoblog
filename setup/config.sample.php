367e1dfcc9310d54d67c6758af85798a9bfd1fce
movsb <anhbk@qq.com>
2015-11-15 04:28:35 +0800

    安装：增加基本安装步骤

--- setup/config.sample.php
+++ setup/config.sample.php
@@ -2,13 +2,13 @@
 
 define('TBPATH',            dirname(__FILE__).'/../');
 
-define('DB_HOST',           'localhost');
-define('DB_USER',           'taoblog');
-define('DB_PASSWORD',       'taoblog');
-define('DB_NAME',           'taoblog');
+define('DB_HOST',           'localhost');               /* 主机名 */
+define('DB_NAME',           'taoblog');                 /* 数据库名 */
+define('DB_USER',           'taoblog');                 /* 数据库用户名 */
+define('DB_PASSWORD',       'taoblog');                 /* 数据库密码 */
 
-define('BAIDU_PUSH_TOKEN',  'baidu_push_token');
+define('BAIDU_PUSH_TOKEN',  'baidu_push_token');        /* 百度推送TOKEN，可无 */
 
-define('PHPMAILER_EMAIL',   'email');
+define('PHPMAILER_EMAIL',   'email');                   /* QQ域名邮箱的邮件地址及用户名 */
 define('PHPMAILER_PASSWD',  'pass');
 
