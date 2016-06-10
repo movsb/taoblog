d66867dc267b3cfa00384fec480ab501c64e1094
movsb <anhbk@qq.com>
2016-06-11 02:05:33 +0800

    去掉遗留的百度PUSH TOKEN配置

--- setup/config.sample.php
+++ setup/config.sample.php
@@ -7,8 +7,6 @@ define('DB_NAME',           'taoblog');                 /* 数据库名 */
 define('DB_USER',           'taoblog');                 /* 数据库用户名 */
 define('DB_PASSWORD',       'taoblog');                 /* 数据库密码 */
 
-define('BAIDU_PUSH_TOKEN',  'baidu_push_token');        /* 百度推送TOKEN，可无 */
-
 define('PHPMAILER_EMAIL',   'email');                   /* QQ域名邮箱的邮件地址及用户名 */
 define('PHPMAILER_PASSWD',  'pass');
 
