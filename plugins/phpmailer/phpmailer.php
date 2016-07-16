a32b3e607d14109cc931da5018af371d7666e498
movsb <anhbk@qq.com>
2016-07-16 23:43:34 +0800

    取消了一处硬编码

--- plugins/phpmailer/phpmailer.php
+++ plugins/phpmailer/phpmailer.php
@@ -3,113 +3,113 @@
 defined('TBPATH') or die('Silence is golden');
 
 function pm_mail($recipient, $nickname, $subject, $body) {
-	require_once('class.smtp.php');
-	require_once('class.phpmailer.php');
+    require_once('class.smtp.php');
+    require_once('class.phpmailer.php');
 
-	$mail = new PHPMailer;
+    $mail = new PHPMailer;
 
-	//$mail->SMTPDebug = 5;									// Enable verbose debug output
+    //$mail->SMTPDebug = 5;                                 // Enable verbose debug output
 
-	$mail->isSMTP();										// Set mailer to use SMTP
-	$mail->CharSet = 'utf-8';
+    $mail->isSMTP();                                        // Set mailer to use SMTP
+    $mail->CharSet = 'utf-8';
 
-	$mail->Host = 'smtp.qq.com';							// Specify main and backup SMTP servers
-	$mail->SMTPAuth = true;									// Enable SMTP authentication
-	$mail->Username = PHPMAILER_EMAIL;  					// SMTP username
-	$mail->Password = PHPMAILER_PASSWD;                     // SMTP password
-	$mail->SMTPSecure = 'ssl';								// Enable TLS encryption, `ssl` also accepted
-	$mail->Port = 465;										// TCP port to connect to
+    $mail->Host = 'smtp.qq.com';                            // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                                 // Enable SMTP authentication
+    $mail->Username = PHPMAILER_EMAIL;                      // SMTP username
+    $mail->Password = PHPMAILER_PASSWD;                     // SMTP password
+    $mail->SMTPSecure = 'ssl';                              // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 465;                                      // TCP port to connect to
 
-	$mail->From = 'blog@twofei.com';
-	$mail->FromName = '博客评论';
-	$mail->addAddress($recipient, $nickname);				// Add a recipient
-	$mail->isHTML(true);									// Set email format to HTML
+    $mail->From = PHPMAILER_EMAIL;
+    $mail->FromName = '博客评论';
+    $mail->addAddress($recipient, $nickname);               // Add a recipient
+    $mail->isHTML(true);                                    // Set email format to HTML
 
-	$mail->Subject = $subject;
-	$mail->Body    = $body;
+    $mail->Subject = $subject;
+    $mail->Body    = $body;
 
-	if(!$mail->send()) {
-		$log = TBPATH.'plugins/phpmailer/error.log';
-		if(($fd = fopen($log, 'a'))) {
-			fwrite($fd, $recipient.': '.$mail->ErrorInfo.' --- '.$body);
-			fclose($fd);
-		}
-		return false;
-	}
+    if(!$mail->send()) {
+        $log = TBPATH.'plugins/phpmailer/error.log';
+        if(($fd = fopen($log, 'a'))) {
+            fwrite($fd, $recipient.': '.$mail->ErrorInfo.' --- '.$body);
+            fclose($fd);
+        }
+        return false;
+    }
 
-	return true;
+    return true;
 }
 
 function pm_notify_admin(&$arg) {
-	global $tbopt;
+    global $tbopt;
     global $tbmain;
 
-	$subject = '[新博文评论] '.$arg->post_title;
-	$link = $tbmain->home.'/?p='.$arg->post_id.'#comments';
+    $subject = '[新博文评论] '.$arg->post_title;
+    $link = $tbmain->home.'/?p='.$arg->post_id.'#comments';
 
-	$body = "<b>您的博文“{$arg->post_title}”有新的评论啦！</b><br><br>";
-	$body .= "<b>链接：</b>{$link}<br>";
-	$body .= "<b>作者：</b>{$arg->author}<br>";
-	$body .= "<b>邮箱：</b>{$arg->email}<br>";
-	$body .= "<b>网址：</b>{$arg->url}<br>";
-	$body .= "<b>时间：</b>{$arg->date}<br>";
-	$body .= "<b>内容：</b>{$arg->content}<br>";
+    $body = "<b>您的博文“{$arg->post_title}”有新的评论啦！</b><br><br>";
+    $body .= "<b>链接：</b>{$link}<br>";
+    $body .= "<b>作者：</b>{$arg->author}<br>";
+    $body .= "<b>邮箱：</b>{$arg->email}<br>";
+    $body .= "<b>网址：</b>{$arg->url}<br>";
+    $body .= "<b>时间：</b>{$arg->date}<br>";
+    $body .= "<b>内容：</b>{$arg->content}<br>";
 
-	return pm_mail($tbopt->get('email'), $tbopt->get('nickname'), $subject, $body);
+    return pm_mail($tbopt->get('email'), $tbopt->get('nickname'), $subject, $body);
 }
 
 function pm_notify_user(&$arg) {
-	global $tbopt;
+    global $tbopt;
     global $tbmain;
 
-	$subject = '[回复评论] '.$arg->post_title;
-	$link = $tbmain->home.'/?p='.$arg->post_id.'#comments';
+    $subject = '[回复评论] '.$arg->post_title;
+    $link = $tbmain->home.'/?p='.$arg->post_id.'#comments';
 
-	$body = "<b>您在博文“{$arg->post_title}”的评论有新的回复啦！</b><br><br>";
-	$body .= "<b>链接：</b>{$link}<br>";
-	$body .= "<b>作者：</b>{$arg->author}<br>";
-	$body .= "<b>时间：</b>{$arg->date}<br>";
-	$body .= "<b>内容：</b>{$arg->content}<br>";
-	$body .= "<br>该邮件为系统自动发出，请勿直接回复该邮件。<br>";
+    $body = "<b>您在博文“{$arg->post_title}”的评论有新的回复啦！</b><br><br>";
+    $body .= "<b>链接：</b>{$link}<br>";
+    $body .= "<b>作者：</b>{$arg->author}<br>";
+    $body .= "<b>时间：</b>{$arg->date}<br>";
+    $body .= "<b>内容：</b>{$arg->content}<br>";
+    $body .= "<br>该邮件为系统自动发出，请勿直接回复该邮件。<br>";
 
-	foreach($arg->parents as &$pc) {
-		pm_mail($pc->email, $pc->author, $subject, $body);
-	}
+    foreach($arg->parents as &$pc) {
+        pm_mail($pc->email, $pc->author, $subject, $body);
+    }
 }
 
 function pm_comment_posted($unused1, $a) {
-	global $tbopt;
-	global $tbpost;
-	global $tbcmts;
+    global $tbopt;
+    global $tbpost;
+    global $tbcmts;
 
-	$admin_email = $tbopt->get('email');
+    $admin_email = $tbopt->get('email');
 
-	$arg = (object)$a;
+    $arg = (object)$a;
 
-	$arg->post_title = $tbpost->get_title((int)$arg->post_id);
+    $arg->post_title = $tbpost->get_title((int)$arg->post_id);
 
-	// 通知站长
-	if($arg->email != $admin_email) {
-		pm_notify_admin($arg);
-	}
+    // 通知站长
+    if($arg->email != $admin_email) {
+        pm_notify_admin($arg);
+    }
 
-	// 通知父评论
-	$parent = (int)$arg->parent;
-	$arg->parents = [];
+    // 通知父评论
+    $parent = (int)$arg->parent;
+    $arg->parents = [];
 
-	while($parent > 0) {
-		$pc = $tbcmts->get_vars('id,author,email', "id=$parent");
-		if(is_object($pc) && $pc->email != $admin_email && $pc->email != $arg->email) {
-			$arg->parents[] = $pc;
-		}
+    while($parent > 0) {
+        $pc = $tbcmts->get_vars('id,author,email', "id=$parent");
+        if(is_object($pc) && $pc->email != $admin_email && $pc->email != $arg->email) {
+            $arg->parents[] = $pc;
+        }
 
         $parent = $pc->id;
-		break; // 暂时不通知父父级评论
-	}
+        break; // 暂时不通知父父级评论
+    }
 
-	if(count($arg->parents)) {
-		pm_notify_user($arg);
-	}
+    if(count($arg->parents)) {
+        pm_notify_user($arg);
+    }
 }
 
 add_hook('comment_posted', 'pm_comment_posted');
