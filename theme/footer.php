671cbd937be08d892e071dca1c793042bbe7f0cf
movsb <anhbk@qq.com>
2016-08-29 02:28:03 +0800

    再换统计代码
    最最开始用 51.la 统计博客园，还不错，比较详细
    后来我博客升级到了 HTTPS，51.la 一直不支持。。。
    所以，换成了 QQ 的 tagjs，发现弱。。
    于是再换，换成 cnzz，一直不想用它的，不知道为什么。。。
    它支持 HTTPS，看起来还不错，先用段时间看看吧。

--- theme/footer.php
+++ theme/footer.php
@@ -12,7 +12,7 @@
 <div class="img-view" id="img-view"><img /><div class="tip"></div></div>
 <div style="display: none;">
 <script src="/theme/scripts/footer.js"></script>
-<script src="https://tajs.qq.com/stats?sId=57851867"></script>
+<script src="//s4.cnzz.com/z_stat.php?id=1260236730&web_id=1260236730"></script>
 </div>
 </body>
 </html>
