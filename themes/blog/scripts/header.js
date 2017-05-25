bb60f8eb85ce49fe9ef810a739c9bc562ec6cffb
movsb <anhbk@qq.com>
2017-05-26 02:28:59 +0800

    module dependent event dispatcher

--- themes/blog/scripts/header.js
+++ themes/blog/scripts/header.js
@@ -6,27 +6,29 @@ function __TaoBlog()
         this._event_maps = {};
     }
 
-    EventDispatcher.prototype._get_map = function(name) {
-        return this._event_maps[name] = this._event_maps[name] || [];
+    EventDispatcher.prototype._get_map = function(module, name) {
+        this._event_maps[module] = this._event_maps[module] || {};
+        this._event_maps[module][name] = this._event_maps[module][name] || [];
+        return this._event_maps[module][name];
     };
 
-    EventDispatcher.prototype.add = function(name, callback) {
-        var listners = this._get_map(name);
+    EventDispatcher.prototype.add = function(module, name, callback) {
+        var listners = this._get_map(module, name);
         listners.push(callback);
     };
 
-    EventDispatcher.prototype.remove = function(name, callback) {
-        var listners = this._get_map(name);
+    EventDispatcher.prototype.remove = function(module, name, callback) {
+        var listners = this._get_map(module, name);
         var index = listners.indexOf(callback);
         if(index != -1) {
             listners.splice(index, 1);
         }
     };
 
-    EventDispatcher.prototype.dispatch = function(name)
+    EventDispatcher.prototype.dispatch = function(module, name)
     {
-        var listners = this._get_map(name);
-        Array.prototype.splice.call(arguments, 0, 1);
+        var listners = this._get_map(module, name);
+        Array.prototype.splice.call(arguments, 0, 2);
         var args = Array.from(arguments);
         listners.forEach(function(callback) {
             callback.apply(null, args);
