3540fdeadbfb45a1f342c797a6ff09ae05edc4fa
movsb <anhbk@qq.com>
2018-04-01 01:11:41 +0800

    change highlight

--- admin/post.php
+++ admin/post.php
@@ -359,9 +359,9 @@ function post_admin_head() {
 <script>
     var renderer = new marked.Renderer();
     renderer.code = function(code, lang) {
-        var beg = '<pre class="code" lang="' + (lang === undefined ? '' : lang) + '">\n';
+        var beg = '<pre class="code"><code class="language-' + (lang === undefined ? 'none' : lang) + '">';
         var text = code.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-        var end = '\n</pre>';
+        var end = '</code></pre>';
         return beg + text + end;
     }
     renderer.hr = function() {
--- plugins/highlight/highlight.php
+++ plugins/highlight/highlight.php
@@ -5,7 +5,33 @@ add_hook('tb_footer', 'hljs_footer');
 function hljs_footer() { 
     global $tbquery;
     if($tbquery->is_singular()) {?>
-    <script type="text/javascript" src="/plugins/highlight/hl.js"></script>
+    <script type="text/javascript" src="/plugins/highlight/prism.js" data-manual></script>
+<script>
+$('pre.code').each(function(_, re, _){
+    var e = $(re);
+    var lang = e.attr('lang');
+    // old style https://stackoverflow.com/a/1318091/3628322
+    if(typeof lang != typeof undefined && lang !== false && e.find('>code').length == 0) {
+        var code = $('<code/>').html(e.html());
+        code.addClass("language-" + lang);
+        e.removeAttr('lang');
+        e.html('');
+        e.append(code);
+    }
+    e.removeClass('code');
+    e.addClass('line-numbers');
+    Prism.highlightAllUnder(re);
+});
+</script>
+<?php }
+}
+
+add_hook('tb_head', 'hljs_header');
+
+function hljs_header() { 
+    global $tbquery;
+    if($tbquery->is_singular()) {?>
+    <link rel="stylesheet" href="/plugins/highlight/prism.css" />
 <?php }
 }
 
