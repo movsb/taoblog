25aa3d79d4c7f17ab0323569d4962e263c607b80
movsb <anhbk@qq.com>
2018-05-11 12:55:23 +0800

    新插件：MathJax，用于显示公式

---
+++ plugins/mathjax/mathjax.js
@@ -0,0 +1,24 @@
+(function() {
+    var mathjaxTimer;
+    mathjaxTimer = setInterval(function(){
+        if(!window.MathJax || !MathJax.isReady) return;
+        clearInterval(mathjaxTimer);
+
+        console.log('mathjax loaded');
+
+        MathJax.Hub.Typeset($('<p>$a$</p>').get(0), function(){
+            console.log('typeset warming-up');
+            $('code:not([class*="lang"])').each(function(_, e) {
+                var html = $(e).html();
+                if(html.startsWith('$') && html.endsWith('$')) {
+                    var wrap = $(html.startsWith('$$') ? '<div/>' : '<span/>')
+                        .css('margin', '3px')
+                        .html(html)[0];
+                    MathJax.Hub.Typeset(wrap);
+                    console.log('Typeset: ', e);
+                    $(e).replaceWith(wrap);
+                }
+            });
+        });
+    }, 1000);
+})();
