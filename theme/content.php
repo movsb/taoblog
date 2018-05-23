10b00ab7d63ce8f72be0ee40a198ae331004a06c
movsb <anhbk@qq.com>
2018-05-23 14:38:52 +0800

    添加版权声明

--- theme/content.php
+++ theme/content.php
@@ -19,7 +19,7 @@
         ?>
         <?php if ($the->type == 'post') { ?>
         <div class="meta clearfix">
-            <span class="item author" itemprop="author" itemscope itemtype="http://schema.org/Person">
+            <p><span class="item author" itemprop="author" itemscope itemtype="http://schema.org/Person">
                 <span itemprop="name"><?php echo htmlspecialchars($tbopt->get('author')); ?></span>
             </span>
             发表于：<?php echo the_meta_date();?>，阅读量：<?php echo $the->page_view; ?><?php
@@ -27,7 +27,8 @@
             if ($s != '') {
                 echo '，标签：<span itemprop="keywords">', $s, '</span>';
             }
-            ?>
+            ?></p>
+            <p>版本声明：若非特别注明，本站所有文章均为作者原创，转载请务必注明原文地址。</p>
         </div>
         <?php } ?>
     </div><!-- end entry -->
--- tools/get-aes2htm.sh
+++ tools/get-aes2htm.sh
@@ -2,5 +2,5 @@
 
 [ -d aes2htm ] || git clone https://github.com/movsb/aes2htm.git || exit 1
 
-cd aes2htm && go build && mv aes2htm ../bin
+cd aes2htm && git pull origin master && go build && mv aes2htm ../bin
 
