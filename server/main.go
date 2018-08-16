62113f94ca38669b9a357b1bb56427e19970b4b7
movsb <anhbk@qq.com>
2018-08-17 01:36:01 +0800

    feat: support http 304

--- server/main.go
+++ server/main.go
@@ -409,12 +409,24 @@ func routerV1(router *gin.Engine) {
 	})
 
 	v1.GET("/posts!rss", func(c *gin.Context) {
+		if ifModified := c.GetHeader("If-Modified-Since"); ifModified != "" {
+			if modified := optmgr.GetDef(gdb, "last_post_time", ""); modified != "" {
+				if ifModified == datetime.Local2Gmt(modified) {
+					c.Status(http.StatusNotModified)
+					return
+				}
+			}
+		}
+
 		rss, err := theFeed(gdb)
 		if err != nil {
 			EndReq(c, err, nil)
 			return
 		}
 		c.Header("Content-Type", "application/xml")
+		if modified := optmgr.GetDef(gdb, "last_post_time", ""); modified != "" {
+			c.Header("Last-Modified", datetime.Local2Gmt(modified))
+		}
 		c.String(http.StatusOK, "%s", rss)
 	})
 
