3a3ee79eefd52a3d77bd2b83273b14093faba168
movsb <anhbk@qq.com>
2019-01-05 00:18:00 +0800

    posts order

--- front/page_posts.go
+++ front/page_posts.go
@@ -1,6 +1,11 @@
 package front
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/movsb/taoblog/modules/utils"
+
 	"github.com/gin-gonic/gin"
 	"github.com/movsb/taoblog/service"
 	"github.com/movsb/taoblog/service/models"
@@ -19,13 +24,25 @@ func (f *Front) GetPagePosts(c *gin.Context) {
 	}
 	footer := &ThemeFooterData{
 		Footer: func() {
-
+			f.render(c.Writer, "page_posts_footer", nil)
 		},
 	}
 	pageData := &PagePostsData{}
+
+	sort := strings.SplitN(c.DefaultQuery("sort", "date.desc"), ".", 2)
+	if len(sort) != 2 {
+		sort = []string{"date", "desc"}
+	}
+	if !utils.StrInSlice([]string{"id", "title", "date", "page_view", "comments"}, sort[0]) {
+		sort[0] = "date"
+	}
+	if !utils.StrInSlice([]string{"asc", "desc"}, sort[1]) {
+		sort[1] = "desc"
+	}
+
 	pageData.Posts = f.server.MustListPosts(&service.ListPostsRequest{
 		Fields:  "id,title,date,page_view,comments",
-		OrderBy: "date DESC",
+		OrderBy: fmt.Sprintf(`%s %s`, sort[0], sort[1]),
 	})
 
 	f.render(c.Writer, "header", header)
