29be65c74d2d3d675c7aa059ac6fb66c462d45b6
movsb <anhbk@qq.com>
2020-10-30 02:19:51 +0800

    优化评论样式

--- server/main.go
+++ server/main.go
@@ -11,13 +11,12 @@ import (
 	"strings"
 	"syscall"
 
-	"github.com/movsb/taoblog/modules/canonical"
-
 	"github.com/mattn/go-sqlite3"
 	"github.com/movsb/taoblog/admin"
 	"github.com/movsb/taoblog/config"
 	"github.com/movsb/taoblog/gateway"
 	"github.com/movsb/taoblog/modules/auth"
+	"github.com/movsb/taoblog/modules/canonical"
 	"github.com/movsb/taoblog/service"
 	inits "github.com/movsb/taoblog/setup/init"
 	"github.com/movsb/taoblog/setup/migration"
