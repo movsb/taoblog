a9bfccc5a8427082a6dcbb6ab17b347f6da6d087
movsb <chkesp@gmail.com>
2025-03-20 23:33:22 +0800

    tidy

--- theme/share/image_viewer/main.go
+++ theme/share/image_viewer/main.go
@@ -8,15 +8,16 @@ import (
 	dynamic "github.com/movsb/taoblog/service/modules/renderers/_dynamic"
 )
 
-//// go:generate sass --style compressed --no-source-map style.scss style.css
+//go:generate sass --style compressed --no-source-map style.scss style.css
 
-// // go:embed zoom-1.0.7-min.iife.js image-view.js
+//go:embed zoom-1.0.7.min.iife.js image-view.js
 var _embed embed.FS
 var _root = os.DirFS(string(dir.SourceAbsoluteDir()))
 
 func init() {
 	dynamic.RegisterInit(func() {
 		const module = `image-viewer`
+		dynamic.WithStyles(module, _embed, _root, `style.css`)
 		dynamic.WithScripts(module, _embed, _root,
 			`zoom-1.0.7.min.iife.js`,
 			`image-view.js`,
