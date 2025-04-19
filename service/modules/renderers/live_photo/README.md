8a648133ddd97bb1576b50a741ed60babd6c7656
movsb <chkesp@gmail.com>
2025-04-20 01:31:43 +0800

    live photo: error prompt

--- service/modules/renderers/live_photo/README.md
+++ service/modules/renderers/live_photo/README.md
@@ -1,6 +1,7 @@
 # LivePhoto - 实况照片
 
-调研：<https://blog.twofei.com/1603/>
+* 调研：<https://blog.twofei.com/1603/>
+* 文档：<https://developer.mozilla.org/en-US/docs/Web/Media/Guides/Autoplay>
 
 使用：完全无感。正常使用图片，如果有同名但后缀为 `.mp4` 的文件，自动渲染成 LivePhoto。
 
--- service/modules/renderers/live_photo/script.js
+++ service/modules/renderers/live_photo/script.js
@@ -4,18 +4,35 @@ document.addEventListener('DOMContentLoaded', () => {
 		const icon = livePhoto.querySelector('.icon');
 		const video = container.querySelector('video');
 		const image = container.querySelector('img');
+		const warning = livePhoto.querySelector('.warning');
+
+		// TODO 优化：等图片可用的时候再把视频显示出来，测试出现
+		// 过视频比图片先加载完成，导致 flicker。
 
 		const start = async (e) => {
 			e.stopPropagation();
 			e.preventDefault();
-			container.classList.add('zoom');
-			video.currentTime = 0;
-			try { await video.play(); }
-			catch(e) { console.log(e); }
+			try {
+				video.currentTime = 0;
+				await video.play();
+				container.classList.add('zoom');
+			}
+			catch(e) {
+				console.log(e);
+				if (e instanceof DOMException) {
+					if (['NotAllowedError','AbortError'].includes(e.name)) {
+						warning.innerText = '浏览器未允许视频自动播放权限，无法播放实况照片。';
+					} else if (['NotSupportedError'].includes(e.name)) {
+						warning.innerText = '视频错误，无法播放。';
+					}
+					warning.classList.add('show');
+				}
+			}
 		};
 
 		const leave = (e) => {
 			container.classList.remove('zoom');
+			warning.classList.remove('show');
 			video.pause();
 		};
 
