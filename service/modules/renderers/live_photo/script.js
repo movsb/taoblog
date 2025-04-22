afc3b32ebdda6cbd2aa499c62061a08fcc5e401f
movsb <chkesp@gmail.com>
2025-04-23 07:41:02 +0800

    LivePhoto: fix webkit

--- service/modules/renderers/live_photo/script.js
+++ service/modules/renderers/live_photo/script.js
@@ -9,21 +9,34 @@ document.addEventListener('DOMContentLoaded', () => {
 		// TODO 优化：等图片可用的时候再把视频显示出来，测试出现
 		// 过视频比图片先加载完成，导致 flicker。
 
+		// fix: 鼠标进入 → 开始加载 → 鼠标离开（加载成功前） → 加载失败。
+		let within = false;
+
 		const start = async (e) => {
 			e.stopPropagation();
 			e.preventDefault();
+
+			within = true;
+
 			try {
+				// 尽可能修复 Safari 无法播放的问题
+				if (/WebKit/.test(navigator.userAgent)) {
+					video.load();
+				}
+
 				video.currentTime = 0;
 				await video.play();
 				container.classList.add('zoom');
 			}
 			catch(e) {
 				console.log(e);
-				if (e instanceof DOMException) {
+				if (within && e instanceof DOMException) {
 					if (['NotAllowedError','AbortError'].includes(e.name)) {
 						warning.innerText = '浏览器未允许视频自动播放权限，无法播放实况照片。';
 					} else if (['NotSupportedError'].includes(e.name)) {
-						warning.innerText = '视频错误，无法播放。';
+						warning.innerText = '浏览器不支持播放此视频格式。';
+					} else {
+						warning.innerText = `其它错误：${e}`;
 					}
 					warning.classList.add('show');
 				}
@@ -33,6 +46,12 @@ document.addEventListener('DOMContentLoaded', () => {
 		const leave = (e) => {
 			container.classList.remove('zoom');
 			warning.classList.remove('show');
+
+			// await play() 可能一直卡住不返回。
+			// 在 pause 之前设置，如果  await play() 还没
+			// 成功返回，就会进入异常处理。
+			within = false;
+
 			video.pause();
 		};
 
