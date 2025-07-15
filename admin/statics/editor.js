68b1773a1b301e1e5c8d55ca7d61f4b08e752d8c
movsb <chkesp@gmail.com>
2025-07-16 00:52:54 +0800

    fix: keep unkept seconds

--- admin/statics/editor.js
+++ admin/statics/editor.js
@@ -300,8 +300,9 @@ class PostFormUI {
 			const day = date.getDate().toString().padStart(2, "0");
 			const hours = date.getHours().toString().padStart(2, "0");
 			const minutes = date.getMinutes().toString().padStart(2, "0");
+			const seconds = date.getSeconds().toString().padStart(2, "0");
 		  
-			return `${year}-${month}-${day}T${hours}:${minutes}`;
+			return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
 		};
 		this.elemTime.value = convertToDateTimeLocalString(new Date(t));
 	}
