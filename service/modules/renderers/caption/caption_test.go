8c6c9ca3a953017b3e78408e2cd171239118ab07
movsb <chkesp@gmail.com>
2025-09-05 00:49:20 +0800

    figure with live photo test

--- service/modules/renderers/caption/caption_test.go
+++ service/modules/renderers/caption/caption_test.go
@@ -1,6 +1,7 @@
 package caption_test
 
 import (
+	"context"
 	"net/url"
 	"strings"
 	"testing"
@@ -10,6 +11,7 @@ import (
 	"github.com/movsb/taoblog/service/modules/renderers"
 	"github.com/movsb/taoblog/service/modules/renderers/caption"
 	"github.com/movsb/taoblog/service/modules/renderers/gold_utils"
+	"github.com/movsb/taoblog/service/modules/renderers/live_photo"
 	"github.com/movsb/taoblog/service/modules/storage"
 	"github.com/movsb/taoblog/setup/migration"
 )
@@ -30,6 +32,21 @@ func TestRender(t *testing.T) {
 		{
 			Markdown: `![](2.avif)`,
 			HTML:     `<figure><img src="2.avif" alt=""/><figcaption><p><strong>bold</strong> <em>italic</em> 说明</p></figcaption></figure>`,
+		},
+		{
+			Markdown: `<p><img src="3.jpg" width=100 height=100></p>`,
+			HTML: `<figure><div class="live-photo" style="width: 100px; height: 100px; aspect-ratio: 1;">
+	<div class="container">
+		<video src="3.webm" playsinline=""></video>
+		<img src="3.jpg" width="100" height="100"/>
+	</div>
+	<div class="icon">
+		<img src="/v3/dynamic/live-photo/live.png" class="static"/>
+		<span>实况</span>
+	</div>
+	<div class="warning" style="opacity: 0;"></div>
+</div>
+<figcaption><p><strong>bold</strong> <em>italic</em> 说明</p></figcaption></figure>`,
 		},
 	}
 
@@ -68,13 +85,45 @@ func TestRender(t *testing.T) {
 		},
 		strings.NewReader("234")),
 	)
+	utils.Must(utils.Write(
+		utils.Must1(dbFS.ForPost(1)),
+		&proto.FileSpec{
+			Path: `3.jpg`,
+			Mode: 0600,
+			Size: 3,
+			Time: 0,
+			Meta: &proto.FileSpec_Meta{
+				Source: &proto.FileSpec_Meta_Source{
+					Format:  proto.FileSpec_Meta_Source_Markdown,
+					Caption: `**bold** *italic* 说明`,
+				},
+			},
+		},
+		strings.NewReader("345")),
+	)
+	utils.Must(utils.Write(
+		utils.Must1(dbFS.ForPost(1)),
+		&proto.FileSpec{
+			Path: `3.webm`,
+			Mode: 0600,
+			Size: 3,
+			Time: 0,
+			Meta: &proto.FileSpec_Meta{
+				Source: &proto.FileSpec_Meta_Source{
+					Format:  proto.FileSpec_Meta_Source_Markdown,
+					Caption: `**bold** *italic* 说明`,
+				},
+			},
+		},
+		strings.NewReader("456")),
+	)
 
 	for i, tc := range testCases {
 		cap := caption.New(gold_utils.NewWebFileSystem(
 			utils.Must1(dbFS.ForPost(1)),
 			utils.Must1(url.Parse(`/`)),
 		))
-		md := renderers.NewMarkdown(cap)
+		md := renderers.NewMarkdown(cap, live_photo.New(context.Background(), gold_utils.NewWebFileSystem(utils.Must1(dbFS.ForPost(1)), &url.URL{Path: `/`})))
 		html, err := md.Render(tc.Markdown)
 		if err != nil {
 			t.Errorf("%d: %v", i, err.Error())
