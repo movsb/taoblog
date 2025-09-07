656392f506eae49e0ca23255866ad589483da95c
movsb <chkesp@gmail.com>
2025-09-07 13:58:04 +0800

    fix bun

--- admin/tldraw/tldraw.go
+++ admin/tldraw/tldraw.go
@@ -2,4 +2,4 @@ package tldraw
 
 import _ "embed"
 
-//go:generate bash -c "bun run build && cp dist/main.js ../statics/tldraw.min.js && cp dist/main.css ../statics/tldraw.min.css"
+//go:generate bash -c "bun install && bun run build && cp dist/main.js ../statics/tldraw.min.js && cp dist/main.css ../statics/tldraw.min.css"
