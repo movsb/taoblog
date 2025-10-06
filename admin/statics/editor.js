a39181c0294d73acee1dfbe4490808ce286b5a64
movsb <chkesp@gmail.com>
2025-10-06 10:12:34 +0800

    fix custom break

--- admin/statics/editor.js
+++ admin/statics/editor.js
@@ -1630,8 +1630,14 @@ class PostFormUI {
 							innerHTML: `✂️ 分隔符`,
 							action: editor => {
 								const date = new Date();
-								let formatted = date.toLocaleString().replaceAll('/', '-');
-								formatted = `\n--- ${formatted} ---\n\n`;
+								const year = date.getFullYear();
+								const month = (date.getMonth() + 1).toString().padStart(2, "0");
+								const day = date.getDate().toString().padStart(2, "0");
+								const hours = date.getHours().toString().padStart(2, "0");
+								const minutes = date.getMinutes().toString().padStart(2, "0");
+								const seconds = date.getSeconds().toString().padStart(2, "0");
+								
+								const formatted = `\n--- ${year}-${month}-${day} ${hours}:${minutes}:${seconds} ---\n\n`;
 								editor.paste(formatted);
 							},
 						},
