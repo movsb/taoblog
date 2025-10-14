c515f874842af6da1ec263f748e9f1414fa5922d
movsb <chkesp@gmail.com>
2025-10-15 02:54:18 +0800

    remove blurhash after load

--- service/modules/renderers/blur_image/script.js
+++ service/modules/renderers/blur_image/script.js
@@ -9,14 +9,22 @@ document.addEventListener('DOMContentLoaded', ()=>{
 	/** @type {NodeListOf<HTMLImageElement>} */
 	const images = document.querySelectorAll('img[data-blurhash]:not(.static)');
 	images.forEach(async img => {
-		if(img.complete) { return; }
+		const removeBlurhash = () => {
+			img.removeAttribute('data-blurhash');
+		};
+
+		if(img.complete) { return removeBlurhash(); }
 
 		let [width, height] = [parseInt(img.width), parseInt(img.height)];
-		if (width <=0 || height <= 0) { return; }
+		if (width <=0 || height <= 0) { return removeBlurhash(); }
 
 		[width, height] = scale(width, height, 1, 32);
 
 		const url = await createBlurImageFromHash(img.dataset.blurhash, width, height, width, height);
+
+		// createBlurImageFromHash 有等待，返回的时候可能已经 complete 了。
+		if(img.complete) { return removeBlurhash(); }
+
 		img.style.backgroundImage = `url("${url}")`;
 		img.style.backgroundRepeat = 'no-repeat';
 		img.style.backgroundSize = 'cover';
@@ -25,6 +33,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
 			img.style.removeProperty('background-image');
 			img.style.removeProperty('background-repeat');
 			img.style.removeProperty('background-size');
+			img.removeAttribute('data-blurhash');
 		});
 	});
 });
