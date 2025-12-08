c4c19d6e7e6741e760c3a67475da78fcfa0443f2
movsb <chkesp@gmail.com>
2025-12-08 13:03:38 +0800

    highlight: better scroll

--- service/modules/renderers/highlight/script.js
+++ service/modules/renderers/highlight/script.js
@@ -2,6 +2,13 @@ function syncCodeScroll(img) {
 	let container = img.parentElement;
 	let tr = container.querySelector(':scope .lntable tr');
 	let td =  container.querySelector(':scope .lntable .lntd:first-child');
-	tr.onscroll = e => td.style.top = `${-tr.scrollTop}px`;
+	let rafId = null;
+	tr.onscroll = () => {
+		if (rafId !== null) return;
+		rafId = requestAnimationFrame(() => {
+			td.style.transform = `translateY(${-tr.scrollTop}px)`;
+			rafId = null;
+		});
+	};
 	img.remove();
 }
