83adab1f8f23bb54fed24d8d8cc772b5a4895e99
movsb <chkesp@gmail.com>
2026-02-07 00:56:49 +0800

    compact byte count iec

--- modules/utils/generic.go
+++ modules/utils/generic.go
@@ -117,14 +117,14 @@ func Join[T any](arr []T, sep string) string {
 func ByteCountIEC(b int64) string {
 	const unit = 1024
 	if b < unit {
-		return fmt.Sprintf("%d B", b)
+		return fmt.Sprintf("%dB", b)
 	}
 	div, exp := int64(unit), 0
 	for n := b / unit; n >= unit; n /= unit {
 		div *= unit
 		exp++
 	}
-	return fmt.Sprintf("%.1f %ciB",
+	return fmt.Sprintf("%.1f%cB",
 		float64(b)/float64(div), "KMGTPE"[exp])
 }
 
